f593cad182744285a29e32299a9ad68a4e49ccd8
Donghao Wu <donghao.wu.us@gmail.com>
2020-11-03 23:22:28 -0500

    add 100% progress bar in final page.

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import Button from '../button/Button';
+import ProgressBar from '../progressBar/ProgressBar'
 
 import './Styles.scss';
 
-const Intro = ({gameStatus, setGameStatus}) => {
-
-    return (
-        <div className="introContainer">
+const Intro = ({ gameStatus, setGameStatus }) => {
+    console.log(gameStatus.message)
+    if (gameStatus.message === 'Great Job! Play again.') {
+        return (
+            <div className="introContainer">
+                <ProgressBar progress={'100%'} />
                 <div className="introContainer--logo">
-                    <img alt="logo" src="assets/logo.png" />            
+                    <img alt="logo" src="assets/logo.png" />
                 </div>
                 <div className="introContainer--message">
                     {gameStatus.message}
                 </div>
-                <Button label="start" handleSubmit={()=> setGameStatus({loadIntro: false})} />
-        </div>
-    )
+                <Button label="start" handleSubmit={() => setGameStatus({ loadIntro: false })} />
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="introContainer">
+                <div className="introContainer--logo">
+                    <img alt="logo" src="assets/logo.png" />
+                </div>
+                <div className="introContainer--message">
+                    {gameStatus.message}
+                </div>
+                <Button label="start" handleSubmit={() => setGameStatus({ loadIntro: false })} />
+            </div>
+        )
+    }
 }
 
 export default Intro
